1c0b60c12ea1b2d7a80dc0f24fb7fb2c604e2546
Ross Lawley <ross.lawley@gmail.com>
2014-08-06 16:06:49 +0100

    Added puppetfile

--- Puppetfile
+++ Puppetfile
@@ -59,3 +59,22 @@ github "xquartz",     "1.1.1"
 
 # Optional/custom modules. There are tons available at
 # https://github.com/boxen.
+
+
+github "iterm2",          "1.0.1"
+github "slate",           "1.0.0"
+github "wget",            "1.0.0"
+
+github "sublime_text_2",  "1.0.1"
+github "intellij",        "1.1.2"
+
+github "virtualbox",      "1.0.1"
+github "vagrant",         "1.0.0"
+
+github "adium",           "1.0.1"
+github "chrome",          "1.0.0"
+github "dropbox",         "1.0.0"
+github "osx",             "1.0.0"
+github "skype",           "1.0.0"
+github "spotify",         "1.0.0"
+github "zsh",             "1.0.0"
---
+++ modules/people/manifests/rozza.pp
@@ -0,0 +1,43 @@
+class people::rozza {
+
+  include iterm2::dev
+  include slate
+  include turn-off-dashboard
+  include wget
+  include zsh
+
+  include adium
+  include chrome
+  include dropbox
+  include skype
+  include spotify
+
+  include sublime_text
+
+  class { 'intellij':
+    edition => 'ultimate',
+  }
+
+  include virtualbox
+  include vagrant
+
+  $my_home  = "/Users/${::luser}"
+  $projects = "${my_home}/vagrant/code/rozza"
+
+  file { $projects:
+    ensure => directory,
+  }
+
+  $dotfiles = "${projects}/dotfiles"
+
+  repository { $dotfiles:
+    source  => 'rozza/dotfiles',
+    require => File[$projects],
+    notify  => Exec['rozza-make-dotfiles'],
+  }
+
+  exec { 'rozza-make-dotfiles':
+    command     => "cd ${dotfiles} && make",
+    refreshonly => true,
+  }
+}
